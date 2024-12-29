9561cf17cf676240465bc66874e16a61c5f73116
Ananth1082 <ananthshetty203@gmail.com>
2024-12-29 15:49:06 +0530

    [feat] added test funcs

--- main.go
+++ main.go
@@ -3,39 +3,68 @@ package main
 import (
 	"fmt"
 	"os"
+	"path"
 	"strings"
 )
 
 // step-1 parse key value pairs
-
-func openFile(fileName string) string {
-	content, err := os.ReadFile(fileName)
-	if err != nil {
-		panic(err)
-	}
-	return string(content)
-}
+// parser should take care of single quotes
 
 func envParser(file string) map[string]string {
 	envVars := make(map[string]string)
 	var key, value strings.Builder
-	isKey := true
-	isEnd := false
+
 	for _, line := range strings.Split(file, "\n") {
+		key.Reset()
+		value.Reset()
+		isKey := true
+		isEnd := false
+		isWithinQuotes := false
+
 		for i, ch := range line {
 			switch ch {
-			case '#':
-				isEnd = true
-			case '=':
-				if key.String() != "" && isKey {
-					isKey = false
+			case '\'':
+				if isWithinQuotes {
+					isWithinQuotes = false
 				} else {
-					fmt.Println("Invalid file format, error in line ", i)
-					fmt.Println("State: ", envVars)
-					panic("Error")
+					isWithinQuotes = true
+				}
+			case '#':
+				if !isWithinQuotes {
+					isEnd = true
+				} else {
+					if isKey {
+						key.WriteRune(ch)
+					} else {
+						value.WriteRune(ch)
+					}
+				}
+			case '=':
+				if !isWithinQuotes {
+					if key.String() != "" && isKey {
+						isKey = false
+					} else {
+						fmt.Println("Invalid file format, error in line ", i)
+						fmt.Println("State: ", envVars)
+						panic("Error")
+					}
+				} else {
+					if isKey {
+						key.WriteRune(ch)
+					} else {
+						value.WriteRune(ch)
+					}
 				}
 			case '\n':
-				isEnd = true
+				if !isWithinQuotes {
+					isEnd = true
+				} else {
+					if isKey {
+						key.WriteRune(ch)
+					} else {
+						value.WriteRune(ch)
+					}
+				}
 			default:
 				if isKey {
 					key.WriteRune(ch)
@@ -50,15 +79,26 @@ func envParser(file string) map[string]string {
 		if key.String() != "" {
 			envVars[key.String()] = value.String()
 		}
-		key.Reset()
-		value.Reset()
-		isKey = true
-		isEnd = false
 	}
 	return envVars
 }
 
+func openFile(fileName string) string {
+	content, err := os.ReadFile(fileName)
+	if err != nil {
+		panic(err)
+	}
+	return string(content)
+}
+func runTests(testNum int) {
+	dir := "test"
+	for i := range testNum {
+		file := fmt.Sprintf(".env_%d", i+1)
+		envFile := openFile(path.Join(dir, file))
+		fmt.Printf("Test %d results: %s\n", i+1, envParser(envFile))
+	}
+}
+
 func main() {
-	cnt := openFile("test/.env")
-	fmt.Println(envParser(cnt))
+	runTests(1)
 }
