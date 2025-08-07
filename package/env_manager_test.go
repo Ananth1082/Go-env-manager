a20d3196a50bb9a7c3eb391259bd1fc6a28d5a51
Ananth1082 <ananthshetty203@gmail.com>
2025-08-07 19:26:02 +0530

    [feat] added support for arrays [test] added tests for parsing and binding logic

---
+++ package/env_manager_test.go
@@ -0,0 +1,73 @@
+package env_manager
+
+import (
+	"slices"
+	"testing"
+)
+
+func assertEqual[T comparable](t *testing.T, actual, expected T, msg string) {
+	if actual != expected {
+		t.Errorf("Assertion error\nExpected %v got %v\n%s", expected, actual, msg)
+	}
+}
+
+func assertCondition(t *testing.T, condition bool, msg string) {
+	if !condition {
+		t.Errorf("Assertion error\nCondition failed: %s", msg)
+	}
+}
+
+// Testing the parsing logic for simple env file
+func TestGetEnvMapForSimpleFile(t *testing.T) {
+	envManger := NewEnvManager("../test_data/simple.env")
+	envMap := envManger.GetEnvMap()
+
+	assertEqual(t, len(envMap), 6, "Invalid number of env variables parsed")
+	assertEqual(t, envMap["APP_NAME"], "MyCoolApp", "Invalid App name from env")
+	assertEqual(t, envMap["APP_PORT"], "8080", "Invalid port from env")
+	t.Log("Env variables: ", len(envMap))
+}
+
+// Testing parsing logic for complex features like multi-line strings, varaibles
+func TestGetEnvMapForComplexFile(t *testing.T) {
+	envManger := NewEnvManager("../test_data/complex.env")
+	envMap := envManger.GetEnvMap()
+
+	assertEqual(t, len(envMap), 21, "Invalid number of env variables parsed")
+	assertEqual(t, envMap["APP_NAME"], "MultiLineApp", "Invalid value for variable APP_NAME from env")
+
+	assertEqual(t, envMap["WELCOME_MESSAGE"], `Welcome to $APP_NAME!
+Environment: production
+Running at http://127.0.0.1:8080`, "Invalid value for variable WELCOME_MESSAGE from env")
+
+	assertEqual(t, envMap["INIT_SQL"], `CREATE TABLE users (
+id SERIAL PRIMARY KEY,
+name TEXT NOT NULL,
+email TEXT UNIQUE NOT NULL
+);`, "Invalid value for variable INIT_SQL from env")
+
+	t.Log("Env variables: ", len(envMap))
+}
+
+type EnvData struct {
+	AppName  string   `env:"APP_NAME"`
+	Version  string   `env:"VERSION"`
+	Options  []string `env:"OPTIONS" env_delim:","`
+	AppCount int      `env:"APP_COUNT" env_def:"69"`
+}
+
+func TestBindEnvForSimpleStruct(t *testing.T) {
+
+	envBinder := new(EnvData)
+
+	envManger := NewEnvManager("../test_data/complex.env")
+	envManger.LoadEnv()
+
+	envManger.BindEnv(envBinder)
+	assertEqual(t, envBinder.AppName, "MultiLineApp", "Invalid AppName")
+	assertEqual(t, envBinder.Version, "1.0.0", "Invalid Version")
+	assertCondition(t, slices.Equal(envBinder.Options, []string{"min", "med", "max"}), "Invalid Options")
+	assertEqual(t, envBinder.AppCount, 69, "Invalid AppCount")
+	t.Log(envBinder)
+
+}
--- package/main.go
+++ package/main.go
@@ -2,23 +2,59 @@ package env_manager
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"reflect"
 	"strconv"
 	"strings"
 )
 
+const (
+	STRUCT_TAG_ENV           = "env"
+	STRUCT_TAG_DEFAULT_VALUE = "env_def"
+	STRUCT_TAG_DELIMITER     = "env_delim"
+	STRUCT_TAG_PREFIX        = "env_prefix"
+	STRUCT_TAG_IGNORE        = "env_ignore"
+)
+
+// EnvManager is a struct that holds the file name and silent mode
+// It is used to manage environment variables from a file
+type EnvManager struct {
+	File   string
+	Silent bool
+}
+
+func NewEnvManager(file string) *EnvManager {
+	if file == "" {
+		panic("file name cannot be empty")
+	}
+	if _, err := os.Stat(file); os.IsNotExist(err) {
+		panic(fmt.Sprintf("file %s does not exist", file))
+	}
+	return &EnvManager{
+		File: file,
+	}
+}
+
+func (e *EnvManager) GetEnvMap() map[string]string {
+	content := openFile(e.File)
+	return envParser(content)
+
+}
+
 // Loads the env variables from an env file
 // supports use of quotes, double quotes, backticks, and variable substituion
-func LoadEnv(fileName string) {
-	cnt := openFile(fileName)
-	envParser(cnt)
+func (e *EnvManager) LoadEnv() {
+	content := openFile(e.File)
+	envMap := envParser(content)
+
+	loadEnvMap(envMap)
 }
 
 // Binds a pointer varaible to env varaibles. The assignment is done based on the value provided in
 // the field tag 'env'
 // example: cat struct{foo string `env:"FOO"`} gets its field foo binded to the varaible 'FOO' 's value
-func BindEnv(envStructPtr interface{}) {
+func (e *EnvManager) BindEnv(envStructPtr any) {
 	// the varaible provided must be a struct ptr
 
 	varType := reflect.TypeOf(envStructPtr)
@@ -26,34 +62,95 @@ func BindEnv(envStructPtr interface{}) {
 		panic("Invalid type of varaible")
 	}
 
-	envStruct := varType.Elem()
+	envStructType := varType.Elem()
 	// loop on each field of struct
-	for i := range envStruct.NumField() {
-		field := envStruct.Field(i)
-		envVarName := field.Tag.Get("env")
+	for i := range envStructType.NumField() {
+		field := envStructType.Field(i)
+		envVarName := field.Tag.Get(STRUCT_TAG_ENV)
 		if envVarName != "" {
 			valStr := os.Getenv(envVarName)
-			if valStr != "" {
-				switch envStruct.Field(i).Type.Kind() {
-				case reflect.String:
-					reflect.ValueOf(envStructPtr).Elem().Field(i).Set(reflect.ValueOf(valStr))
-				case reflect.Int:
-					valInt, err := strconv.Atoi(valStr)
-					if err != nil {
-						errMsg := fmt.Sprintf("error: env variable %s is not a integer", envVarName)
-						panic(errMsg)
-					}
-					reflect.ValueOf(envStructPtr).Elem().Field(i).Set(reflect.ValueOf(valInt))
+			if valStr == "" {
+				if valStr = envStructType.Field(i).Tag.Get(STRUCT_TAG_DEFAULT_VALUE); valStr == "" {
+					panic(fmt.Sprintf("error: env variable %s not found", envVarName))
 				}
+			}
+			if value, err := castString(valStr, field.Type, field.Tag.Get(STRUCT_TAG_DELIMITER)); err != nil {
+				log.Println(err)
 			} else {
-				errMsg := fmt.Sprintf("error: env variable %s not found", envVarName)
-				panic(errMsg)
+				reflect.ValueOf(envStructPtr).Elem().Field(i).Set(value)
 			}
 		}
 	}
 }
 
-func subValues(str string) string {
+func castString(value string, target reflect.Type, delim string) (reflect.Value, error) {
+	var err error
+	var castValue any
+
+	switch target.Kind() {
+	case reflect.String:
+		castValue = value
+	case reflect.Int:
+		castValue, err = strconv.Atoi(value)
+	case reflect.Int8:
+		castValue, err = strconv.ParseInt(value, 10, 8)
+	case reflect.Int16:
+		castValue, err = strconv.ParseInt(value, 10, 16)
+	case reflect.Int32:
+		castValue, err = strconv.ParseInt(value, 10, 32)
+	case reflect.Int64:
+		castValue, err = strconv.ParseInt(value, 10, 64)
+	case reflect.Uint:
+		castValue, err = strconv.ParseUint(value, 10, 0)
+	case reflect.Uint8:
+		castValue, err = strconv.ParseUint(value, 10, 8)
+	case reflect.Uint16:
+		castValue, err = strconv.ParseUint(value, 10, 16)
+	case reflect.Uint32:
+		castValue, err = strconv.ParseUint(value, 10, 32)
+	case reflect.Uint64:
+		castValue, err = strconv.ParseUint(value, 10, 64)
+	case reflect.Uintptr:
+		castValue, err = strconv.ParseUint(value, 10, 0)
+	case reflect.Complex64:
+		castValue, err = strconv.ParseComplex(value, 64)
+	case reflect.Complex128:
+		castValue, err = strconv.ParseComplex(value, 128)
+	case reflect.Bool:
+		castValue, err = strconv.ParseBool(value)
+	case reflect.Float32:
+		castValue, err = strconv.ParseFloat(value, 32)
+	case reflect.Float64:
+		castValue, err = strconv.ParseFloat(value, 64)
+	case reflect.Array, reflect.Slice:
+		// delimter if not provided is ','
+		if delim == "" {
+			delim = ","
+		}
+		// split the string by the delimiter and convert each part to the target type
+		parts := strings.Split(value, delim)
+		slice := reflect.MakeSlice(reflect.SliceOf(reflect.TypeOf(value)), len(parts), len(parts))
+		for i, part := range parts {
+			part = strings.TrimSpace(part)
+			elemValue, elemErr := castString(part, target.Elem(), delim)
+			if elemErr != nil {
+				return reflect.Value{}, elemErr
+			}
+			slice.Index(i).Set(elemValue)
+		}
+		return slice, nil
+	case reflect.Chan, reflect.Func:
+	// ignored
+	default:
+		err = fmt.Errorf("error: type %s not supported", target.Kind())
+	}
+	if err != nil {
+		return reflect.Value{}, err
+	}
+	return reflect.ValueOf(castValue), nil
+}
+
+func subValues(envMap map[string]string, str string) string {
 	start, open, close := 0, 0, 0
 	variable := ""
 	n := len(str)
@@ -71,7 +168,7 @@ func subValues(str string) string {
 		}
 		close += open
 		variable = str[open:close]
-		val := os.Getenv(variable)
+		val := getEnv(envMap, variable)
 		if val == "" {
 			errMsg := fmt.Sprintf("Error: undefined varaible %s", variable)
 			panic(errMsg)
@@ -83,7 +180,16 @@ func subValues(str string) string {
 	return str
 }
 
-func envParser(file string) {
+func getEnv(envMap map[string]string, key string) string {
+	if value, ok := envMap[key]; ok {
+		return value
+	} else {
+		return os.Getenv(key)
+	}
+}
+
+func envParser(file string) map[string]string {
+	envMap := make(map[string]string)
 	var key, value strings.Builder
 	isWithinQuotes := false
 	quoteRune := rune(-1)
@@ -165,12 +271,13 @@ func envParser(file string) {
 		}
 		if !isWithinQuotes && key.String() != "" {
 			if quoteRune == '\'' {
-				os.Setenv(key.String(), value.String())
+				envMap[key.String()] = value.String()
 			} else {
-				os.Setenv(key.String(), subValues(value.String()))
+				envMap[key.String()] = subValues(envMap, value.String())
 			}
 		}
 	}
+	return envMap
 }
 
 func openFile(fileName string) string {
@@ -179,4 +286,13 @@ func openFile(fileName string) string {
 		panic(err)
 	}
 	return string(content)
+}
+
+func loadEnvMap(envMap map[string]string) {
+	for key, value := range envMap {
+		if err := os.Setenv(key, value); err != nil {
+			panic(fmt.Sprintf("Error setting env variable %s: %v", key, err))
+		}
+	}
+
 }
