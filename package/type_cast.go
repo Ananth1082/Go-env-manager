0c5dc7cf411d32552cd87c3ef5e8e7ca29379968
Ananth1082 <ananthshetty203@gmail.com>
2025-08-15 09:33:44 +0530

    [refact]

---
+++ package/type_cast.go
@@ -0,0 +1,87 @@
+package env_manager
+
+import (
+	"fmt"
+	"reflect"
+	"strconv"
+	"strings"
+)
+
+func castString(value string, target reflect.Type, delim string) (reflect.Value, error) {
+	var castValue reflect.Value
+	var err error
+	if isPrimitive(target) {
+		castValue, err = castStringToPrimitive(value, target)
+	} else if target.Kind() == reflect.Slice {
+		castValue, err = castStringToSlice(value, target.Elem(), delim)
+	} else {
+		panic(fmt.Sprintf("error: type %s not supported", target.Kind()))
+	}
+	if err != nil {
+		return reflect.Value{}, err
+	} else {
+		return castValue, nil
+	}
+}
+
+func castStringToSlice(value string, targetElement reflect.Type, delim string) (reflect.Value, error) {
+	parts := strings.Split(value, delim)
+	slice := reflect.MakeSlice(reflect.SliceOf(targetElement), len(parts), len(parts))
+	for i, part := range parts {
+		part = strings.TrimSpace(part)
+		elemValue, elemErr := castStringToPrimitive(part, targetElement)
+		if elemErr != nil {
+			return reflect.Value{}, elemErr
+		}
+		slice.Index(i).Set(elemValue)
+	}
+	return slice, nil
+}
+
+func castStringToPrimitive(value string, target reflect.Type) (reflect.Value, error) {
+	var err error
+	var castValue any
+
+	switch target.Kind() {
+	case reflect.String:
+		castValue = value
+	case reflect.Int:
+		castValue, err = strconv.Atoi(value)
+	case reflect.Int8:
+		castValue, err = strconv.ParseInt(value, 10, 8)
+	case reflect.Int16:
+		castValue, err = strconv.ParseInt(value, 10, 16)
+	case reflect.Int32:
+		castValue, err = strconv.ParseInt(value, 10, 32)
+	case reflect.Int64:
+		castValue, err = strconv.ParseInt(value, 10, 64)
+	case reflect.Uint:
+		castValue, err = strconv.ParseUint(value, 10, 0)
+	case reflect.Uint8:
+		castValue, err = strconv.ParseUint(value, 10, 8)
+	case reflect.Uint16:
+		castValue, err = strconv.ParseUint(value, 10, 16)
+	case reflect.Uint32:
+		castValue, err = strconv.ParseUint(value, 10, 32)
+	case reflect.Uint64:
+		castValue, err = strconv.ParseUint(value, 10, 64)
+	case reflect.Uintptr:
+		castValue, err = strconv.ParseUint(value, 10, 0)
+	case reflect.Complex64:
+		castValue, err = strconv.ParseComplex(value, 64)
+	case reflect.Complex128:
+		castValue, err = strconv.ParseComplex(value, 128)
+	case reflect.Bool:
+		castValue, err = strconv.ParseBool(value)
+	case reflect.Float32:
+		castValue, err = strconv.ParseFloat(value, 32)
+	case reflect.Float64:
+		castValue, err = strconv.ParseFloat(value, 64)
+	default:
+		err = fmt.Errorf("error: type %s not supported", target.Kind())
+	}
+	if err != nil {
+		return reflect.Value{}, err
+	}
+	return reflect.ValueOf(castValue), nil
+}
---
+++ package/util.go
@@ -0,0 +1,92 @@
+package env_manager
+
+import (
+	"fmt"
+	"os"
+	"reflect"
+	"strings"
+	"unicode"
+)
+
+func getEnv(envMap map[string]string, key string) string {
+	if value, ok := envMap[key]; ok {
+		return value
+	} else {
+		return os.Getenv(key)
+	}
+}
+
+func openFile(fileName string) string {
+	content, err := os.ReadFile(fileName)
+	if err != nil {
+		panic(err)
+	}
+	return string(content)
+}
+
+func loadEnvMap(envMap map[string]string) {
+	for key, value := range envMap {
+		if err := os.Setenv(key, value); err != nil {
+			panic(fmt.Sprintf("Error setting env variable %s: %v", key, err))
+		}
+	}
+
+}
+
+func getNameFromTag(tags []string, fieldName string) string {
+	if len(tags) == 0 {
+		return ""
+	}
+	for _, part := range tags {
+		if part != "" && !isKeyWord(part) {
+			return part
+		}
+	}
+
+	//fallback to pascale to snake case if no env tag is provided
+	fallback := pascaleToSSnakeCase(fieldName)
+	return fallback
+}
+
+func isKeyWord(tagEntry string) bool {
+	switch tagEntry {
+	case STRUCT_KEYWORD_IGNORE:
+		return true
+	default:
+		return false
+	}
+}
+
+func pascaleToSSnakeCase(str string) string {
+	var result strings.Builder
+	for i, ch := range str {
+		if unicode.IsUpper(ch) && i > 0 {
+			result.WriteRune('_')
+		}
+		result.WriteRune(unicode.ToUpper(ch))
+	}
+	return result.String()
+}
+
+// IsPrimitive checks whether the type is a Go primitive type.
+func isPrimitive(t reflect.Type) bool {
+	switch t.Kind() {
+	case reflect.Bool,
+		reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
+		reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr,
+		reflect.Float32, reflect.Float64,
+		reflect.Complex64, reflect.Complex128,
+		reflect.String:
+		return true
+	default:
+		return false
+	}
+}
+
+func getDelim(field reflect.StructField) string {
+	delim := field.Tag.Get(STRUCT_TAG_DELIMITER)
+	if delim == "" {
+		delim = ","
+	}
+	return delim
+}
